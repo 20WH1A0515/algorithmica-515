66213739f02d22ef2dd65a4a3f892c8f0c45100c
Vennela Reddy <20wh1a0515@bvrithyderabad.edu.in>
2022-12-03 15:39:14 +0530

    partially

---
+++ experience_with_expression/mailadd.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name = "vennela";
+    string address = "hyderabad";
+    cout<<name<<" "<<address;
+    return 0;
+}
---
+++ experience_with_expression/roomarea.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float l,w;
+    cout<<"enter the length:";
+    cin>>l;
+    cout<<"enter the width:";
+    cin>>w;
+    cout<<"area of the room:"<<l*w;
+    return 0;
+}

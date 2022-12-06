470762b4efe9c52321b0b071aa39be89cf14790c
Vennela Reddy <20wh1a0515@bvrithyderabad.edu.in>
2022-12-06 15:44:50 +0530

    partially done

---
+++ experience_with_expression/compoundinterest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float dep,amt,interest,i;
+    cout<<"enter the deposit number:";
+    cin>>dep;
+    for(i=1;i<=3;i++){
+        interest=dep*0.04;
+        amt=dep+interest;
+    cout<<"compound interest is:"<<amt<<"\n";
+    dep=amt;
+    }
+    return 0;
+}

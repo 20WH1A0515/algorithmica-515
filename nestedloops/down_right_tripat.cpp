fbd44f7bcc0d9212c041fe66df7fe94177715514
Vennela Reddy <20wh1a0515@bvrithyderabad.edu.in>
2022-12-15 20:45:01 +0530

    partial

---
+++ nestedloops/down_right_tripat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"enter a number:";
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i;j++){
+            cout<<"*";
+        }
+         cout<<"\n";
+    }
+    
+    return 0;
+}

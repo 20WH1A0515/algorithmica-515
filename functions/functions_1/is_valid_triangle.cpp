0177b07ab4bd833e929c392c84746c3f1c7d6481
Vennela Reddy <20wh1a0515@bvrithyderabad.edu.in>
2022-12-19 14:28:47 +0530

    Create is_valid_triangle.cpp

---
+++ functions/functions_1/is_valid_triangle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int isValidTriangle(int a,int b,int c){
+    if((c>=a+b)||(a>=b+c)||(b>=a+c)){
+        return false;
+    }
+    else{
+        if((a<=0)||(b<=0)||(c<=0)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+int main(){
+    int a,b,c;
+    cout<<"enter the lengths of a,b and c:";
+    cin>>a>>b>>c;
+    cout<<isValidTriangle(a,b,c);
+}
